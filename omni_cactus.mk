75dbac21d6f2251a152abd08ebc4c8c150e946b2
megafon929 <yarikpopkov1@gmail.com>
2021-07-13 23:26:32 +0500

    Fix 3/64 and 4/64

--- omni_cactus.mk
+++ omni_cactus.mk
@@ -14,6 +14,12 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_cactus.mk
+# Inherit from our custom product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
+PRODUCT_DEVICE := cactus
+PRODUCT_NAME := omni_cactus
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Redmi 6A
+PRODUCT_MANUFACTURER := Xiaomi
